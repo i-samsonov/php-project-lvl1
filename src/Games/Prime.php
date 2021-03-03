58e3b2f5e1a7eb314fd0f76d6d444f181dcabe10
ivan <regspamonly@yandex.ru>
2021-03-04 02:18:41 +0300

    update brain-prime

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -10,7 +10,18 @@ function legend()
 function run()
 {
     $x = rand(1, 101);
-    $correct = \gmp_prob_prime($x) === 2 ? "yes" : "no";
+//    $correct = gmp_prob_prime($x) === 2 ? "yes" : "no";
+    $correct = $x > 1 && isPrime($x) ? "yes" : "no";
 
     return ['question' => $x, 'correct' => $correct];
+}
+
+function isPrime($n)
+{
+    for ($i = 2; $i < $n; $i++) {
+        if ($n % $i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
